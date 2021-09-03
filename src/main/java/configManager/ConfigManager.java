013e8f542d269aa728c080a4b2c8e0df488ba906
laura.darnell <laura.darnell@fanduel.com>
2021-09-03 11:17:41 +0100

    creating our config and config reader object

---
+++ src/main/java/configManager/ConfigManager.java
@@ -0,0 +1,26 @@
+package configManager;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigManager {
+    private static final Properties config;
+
+    static {
+        config = new Properties();
+        try {
+            config.load(new FileReader("src/main/config.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static String employeeFileLocation(){
+        return config.getProperty("employeeFileLocation");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ConfigManager.employeeFileLocation());
+    }
+}
